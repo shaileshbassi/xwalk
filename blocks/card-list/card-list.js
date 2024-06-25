c768f4856a885a862231b4dd985638731d03979c
shailesh <shaileshbassi8@gmail.com>
2024-06-25 15:29:57 +0530

    initial testing

--- blocks/card-list/card-list.js
+++ blocks/card-list/card-list.js
@@ -1,4 +1,9 @@
 export default function decorate(block) {
   const cardListContainer = document.createElement("div");
   cardListContainer.classList.add("cardList-container-section");
+
+  [...block.children].forEach((row) => {
+    console.log("inside block children");
+    console.log(row);
+  });
   }
--- blocks/card/card.js
+++ blocks/card/card.js
@@ -1,7 +1,10 @@
 export default function decorate(block) {
 
+  console.log("inside card block");
+  console.log(block);
   const cardDiv = document.createElement('div');
   const title = cardDiv.createElement('div');
   div.className = 'card-title';
   block.append(cardDiv);
+  block.innerHTML = blockAuthor.innerHTML;
 }
