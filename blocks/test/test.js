2ccc119d1fc194328f6dd11c0ae3c9a53eb3c5b8
shailesh <shaileshbassi8@gmail.com>
2024-06-25 16:39:45 +0530

    initial testing

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -1,4 +1,18 @@
 export default function decorate(block) {
     console.log("inside the test block");
+    const titleElement = block.querySelector('div:nth-child(1) > p');
+    const title = titleElement.textContent.trim();
+
+
+    const descriptionElement = block.querySelector('div:nth-child(1) > ol');
+
     console.log(block);
+
+    block.parentElement.classList.add("parent_test");
+    block.innerHTML = `
+        <div class="test">
+            <h2>${title}</h2>
+             ${descriptionElement}
+        </div>
+    `;
     }
