14239bd637d3d7d83bc0ee32147bbeb46d799c33
shailesh <shaileshbassi8@gmail.com>
2024-07-23 16:17:25 +0530

    testing post commenting the code

--- blocks/card-list/card-list.js
+++ blocks/card-list/card-list.js
@@ -1,11 +1,11 @@
-import {fetchPlaceholders} from '../../scripts/aem.js';
+// import {fetchPlaceholders} from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  const placeholders = fetchPlaceholders();
-  placeholders.forEach((row) => {
-    console.log("inside placeholder block")
-    console.log(row);
-  });
+  // const placeholders = fetchPlaceholders("en");
+  // placeholders.forEach((row) => {
+  //   console.log("inside placeholder block")
+  //   console.log(row);
+  // });
   const cardListContainer = document.createElement("div");
   cardListContainer.classList.add("cardList-container-section");
 
